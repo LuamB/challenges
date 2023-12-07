379495224d10882dc4a4bbbdd5e937f00d7ce863
Luam Belay <luam.belay@gmail.com>
2023-12-07 21:09:09 +0100

    Track your savings challenge completed

--- js-variables-and-numbers/index.js
+++ js-variables-and-numbers/index.js
@@ -4,7 +4,13 @@ const circleRadius = 5;
 const circleArea = Math.PI * Math.pow(circleRadius, 2);
 const circumference = 2 * Math.PI * circleRadius;
 
-console.log(`For a circle with a radius of 5 units:
+console.log(
+  `For a circle with a radius of 5 units:
 
--   The area is approximately 78.54 square units.
--   The circumference is approximately 31.42 units.`);
+-   The area is approximately ` +
+    circleArea.toFixed(2) +
+    ` square units.
+-   The circumference is approximately ` +
+    circumference.toFixed(2) +
+    ` units.`
+);

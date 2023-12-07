7f6b7c0146274e66efa2e5d4363433b580403089
Luam Belay <luam.belay@gmail.com>
2023-12-07 21:25:57 +0100

    Tip calculator challenge completed

---
+++ js-variables-and-numbers/tip-calculator.js
@@ -0,0 +1,7 @@
+// Tip Calculator
+const mealCost = 30;
+let tipPercentage = 0.1;
+let tipAmount = mealCost * tipPercentage;
+let totalCost = mealCost + tipAmount;
+
+console.log("// The total cost of your bill is: " + totalCost + " euros");

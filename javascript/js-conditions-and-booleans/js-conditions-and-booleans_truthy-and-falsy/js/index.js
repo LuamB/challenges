303e48869fc5b98d8fe6b1dfd4098e2df9d01544
Luam Belay <luam.belay@gmail.com>
2023-12-10 23:40:30 +0100

    Truthy or falsy completed, but no PR required

--- javascript/js-conditions-and-booleans/js-conditions-and-booleans_truthy-and-falsy/js/index.js
+++ javascript/js-conditions-and-booleans/js-conditions-and-booleans_truthy-and-falsy/js/index.js
@@ -1,2 +1,35 @@
-const truthyOrFalsy = Boolean(3 >= 2);
-console.log("The expression is " + truthyOrFalsy);
+// const truthyOrFalsy = Boolean(3 >= 2);
+// console.log("The expression is " + truthyOrFalsy);
+
+let truthyOrFalsy = Boolean(2 == "2");
+console.log("Expression is ", truthyOrFalsy);
+
+truthyOrFalsy = Boolean(2 === "2");
+console.log("Expression is ", truthyOrFalsy);
+
+truthyOrFalsy = Boolean(2 === 2);
+console.log("Expression is ", truthyOrFalsy);
+
+truthyOrFalsy = Boolean("hello world");
+console.log("Expression is", truthyOrFalsy);
+
+truthyOrFalsy = Boolean("");
+console.log("Expression is", truthyOrFalsy);
+
+truthyOrFalsy = Boolean(true);
+console.log("Expression is", truthyOrFalsy);
+
+truthyOrFalsy = Boolean(0);
+console.log("Expression is", truthyOrFalsy);
+
+truthyOrFalsy = Boolean(-50);
+console.log("Expression is", truthyOrFalsy);
+
+truthyOrFalsy = Boolean(50);
+console.log("Expression is", truthyOrFalsy);
+
+truthyOrFalsy = Boolean(5 > 1);
+console.log("Expression is", truthyOrFalsy);
+
+truthyOrFalsy = Boolean("1 > 10");
+console.log("Expression is", truthyOrFalsy);

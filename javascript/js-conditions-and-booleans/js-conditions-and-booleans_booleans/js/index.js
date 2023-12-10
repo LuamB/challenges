f943985d43d884a4938a787d4acbf898fc0d613e
Luam Belay <luam.belay@gmail.com>
2023-12-10 23:01:32 +0100

    Booleans challenge completed

--- javascript/js-conditions-and-booleans/js-conditions-and-booleans_booleans/js/index.js
+++ javascript/js-conditions-and-booleans/js-conditions-and-booleans_booleans/js/index.js
@@ -1,3 +1,28 @@
 console.clear();
 
+// 1.
+console.log('1. 2 == "2"', true);
+// 2.
+console.log('2. 2 === "2"', false);
+// 3.
+console.log("3. 2 === 2", true);
+// 4.
+console.log("4. hello world", true);
+// 5.
+console.log('5. ""', false);
+// 6.
+console.log("6. true", true);
+// 7.
+console.log("7. 0", false);
+// 8.
+console.log("8. -50", true);
+// 9.
+console.log("9. 50", true);
+// 10.
+console.log("10. 5 > 1", true);
+// 11.
+console.log('11. "1 > 10"', true);
+// 12.
+console.log('12. "false"', true);
+
 console.log(Boolean("Try it out"));

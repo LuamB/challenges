b9771d862b0c39292253de110e9901cb84d8d8d8
Luam Belay <luam.belay@gmail.com>
2023-12-10 22:09:25 +0100

    Dark mode challenge completed

--- javascript/js-basics/js-basics_dark-mode/js/index.js
+++ javascript/js-basics/js-basics_dark-mode/js/index.js
@@ -1,3 +1,26 @@
 console.clear();
 
 const bodyElement = document.querySelector('[data-js="body"]');
+
+//Add interactivity
+
+// Query 'Switch to dark mode'-button
+const darkButton = document.querySelector('[data-js="dark-mode-button"]');
+
+darkButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});
+
+// Query 'Switch to light mode'-button
+const lightButton = document.querySelector('[data-js="light-mode-button"]');
+
+lightButton.addEventListener("click", () => {
+  bodyElement.classList.remove("dark");
+});
+
+// Query 'Toggle'-button
+const toggleButton = document.querySelector('[data-js="toggle-button"]');
+
+toggleButton.addEventListener("click", () => {
+  bodyElement.classList.add("dark");
+});

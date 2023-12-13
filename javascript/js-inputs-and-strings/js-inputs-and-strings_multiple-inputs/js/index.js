f7b834b16586f7d5fb8b2f73f49cc5a311dac1ac
Luam Belay <luam.belay@gmail.com>
2023-12-14 00:48:55 +0100

    One button, multiple clicks

--- javascript/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
+++ javascript/js-inputs-and-strings/js-inputs-and-strings_multiple-inputs/js/index.js
@@ -1,3 +1,4 @@
+console.clear;
 /*
 
 Important: Create a fork for each sub challenge!
@@ -27,9 +28,58 @@ const button = document.querySelector("[data-js=button]");
 //   secondInput.value = firstValue.toUpperCase();
 // });
 
+// button.addEventListener("click", () => {
+//   // Third click
+//   const firstValue = firstInput.value;
+//   firstInput.value = secondInput.value;
+//   secondInput.value = firstValue;
+// });
+
+// One buttion, multiple inputs
+
+// Set counter
+let counter = 0;
+
+// Declaration of firstValue outside the switch statement
+const firstValue;
+
 button.addEventListener("click", () => {
-  // Third click
-  const firstValue = firstInput.value;
-  firstInput.value = secondInput.value;
-  secondInput.value = firstValue;
+  // Increment the counter
+  counter++;
+
+  // Switch between different actions based on the counter value
+  switch (counter) {
+    case 1:
+      // Initialize firstValue inside the case
+      firstValue = firstInput.value;
+
+      // Copy the value of firstValue into the second input field
+      secondInput.value = firstValue;
+
+      // Do nothing else with the counter or button text
+      break;
+
+    case 2:
+      // Initialize firstValue inside the case
+      firstValue = firstInput.value;
+
+      // Convert the value of firstValue to uppercase and copy it into the second input field
+      secondInput.value = firstValue.toUpperCase();
+
+      // Do nothing else with the counter or button text
+      break;
+
+    default:
+      // Initialize firstValue inside the case
+      firstValue = firstInput.value;
+
+      // Swap the values of the two input fields
+      const temp = firstInput.value;
+      firstInput.value = secondInput.value;
+      secondInput.value = temp;
+
+      // Increment the counter
+      counter++;
+      break;
+  }
 });

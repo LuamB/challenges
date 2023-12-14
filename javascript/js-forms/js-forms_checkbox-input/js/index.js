2ca33317206bca6bcb5e97674b8635b626b9d61e
Luam Belay <luam.belay@gmail.com>
2023-12-14 15:37:51 +0100

    Made notes in checkbox-input example

--- javascript/js-forms/js-forms_checkbox-input/js/index.js
+++ javascript/js-forms/js-forms_checkbox-input/js/index.js
@@ -4,10 +4,16 @@ const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
 
+/* Function setting hidden attribute to false with empty string "", 
+which means "" will be displayed. .setAttribute("hidden", false) would have the same effect.
+Note: the default of hidden for all HTML elements is false */
 function hideTosError() {
   tosError.setAttribute("hidden", "");
 }
 
+/* Function removing hidden attribute. Note: Calling removeAttribute("hidden") on an element 
+with "hidden" set to false (default) has the same effect as calling setAttribute("hidden", true). 
+Both of these actions will cause the element to be hidden. */
 function showTosError() {
   tosError.removeAttribute("hidden");
 }
@@ -35,6 +41,7 @@ form.addEventListener("submit", (event) => {
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
+  /* alert() function is used to display a pop-up window with a message.  */
   alert("Form submitted");
 });
 

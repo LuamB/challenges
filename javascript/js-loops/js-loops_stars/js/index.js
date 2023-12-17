5d4c7cccfd50d7f637062a0934ce0e5793da4b31
Luam Belay <luam.belay@gmail.com>
2023-12-17 23:41:22 +0100

    Rating stars

--- javascript/js-loops/js-loops_stars/js/index.js
+++ javascript/js-loops/js-loops_stars/js/index.js
@@ -2,13 +2,34 @@ console.clear();
 
 const starContainer = document.querySelector('[data-js="star-container"]');
 
-function renderStars() {
+function renderStars(filledStars) {
   // reset the star container before rerendering stars
   starContainer.innerHTML = "";
 
   //--v-- your code here --v--
 
+  for (i = 1; i <= 5; i++) {
+    const starImg = document.createElement("img");
+    starImg.src =
+      filledStars >= i ? "./assets/star-filled.svg" : "./assets/star-empty.svg";
+    // add styling
+    // starImg.classList.add("img");
+
+    let index = i;
+
+    // Add a click event listener to each star
+    starImg.addEventListener("click", () => {
+      // Re-render stars based on the clicked star's position (i)
+      renderStars(index);
+    });
+
+    starContainer.appendChild(starImg);
+  }
+
+  // add styling
+  // starContainer.classList.add("star-container");
+
   //--^-- your code here --^--
 }
 
-renderStars();
+renderStars(0);

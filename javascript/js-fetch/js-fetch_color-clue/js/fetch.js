b517fc121ab382da538e5841b5d8b758b53a42c6
Luam Belay <luam.belay@gmail.com>
2024-01-08 21:45:59 +0100

    Added try/catch and handling of bad response

--- javascript/js-fetch/js-fetch_color-clue/js/fetch.js
+++ javascript/js-fetch/js-fetch_color-clue/js/fetch.js
@@ -19,13 +19,20 @@ export async function fetchNewColor() {
 
   // --v-- your code here --v--
   // const colorData = await fetch(colorApiUrl).then((response) => response.json());
-  const response = await fetch(colorApiUrl);
-  const colorData = await response.json();
-  console.log("colorData", colorData);
+  try {
+    const response = await fetch(colorApiUrl);
+    if (!response.ok) {
+      console.error("Bad request -> ", response.status);
+      return;
+    }
+    const colorData = await response.json();
+    const colorHexCode = colorData.name.closest_named_hex;
+    const colorName = colorData.name.value;
 
-  const colorHexCode = colorData.name.closest_named_hex;
-  const colorName = colorData.name.value;
+    setColorToGuess(colorHexCode, colorName);
+  } catch (error) {
+    console.log("Something went wrong -> ", error);
+  }
 
-  setColorToGuess(colorHexCode, colorName);
   // --^-- your code here --^--
 }

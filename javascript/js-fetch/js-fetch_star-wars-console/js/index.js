221bb8ad1a843437a57fe0d20db4787deb038a4a
Luam Belay <luam.belay@gmail.com>
2024-01-08 17:29:50 +0100

    Recap session

--- javascript/js-fetch/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -5,21 +5,38 @@ const url = "https://swapi.dev/api/people";
 async function fetchData() {
   try {
     const response = await fetch(url);
-    const data = await response.json();
-    console.log("data: ", data);
+    if (!response.ok) {
+      console.error("Bad request ->", response.status);
+      return;
+    }
+    // console.log("respone: ", response);
 
-    const characters = data.results;
-    let charName = "";
-    let charEyeCol = "";
+    // const data = await response.json();
+    // console.log("data: ", data);
 
-    characters.forEach((character) => {
-      charName = character.name;
-      charEyeCol = character.eye_color;
+    // const characters = data.results;
+    // let charName = "";
+    // let charEyeCol = "";
 
-      console.log(`${charName} has ${charEyeCol} eyes`);
+    // characters.forEach((character) => {
+    //   charName = character.name;
+    //   charEyeCol = character.eye_color;
+
+    //   console.log(`${charName} has ${charEyeCol} eyes`);
+    // });
+    // Marcel's solution
+    const characters = await response.json();
+
+    characters.results.forEach((character) => {
+      console.log("----------------");
+      console.log("name -->", character.name);
+      console.log("height -->", character.height);
+      if (character.name === "R2-D2") {
+        console.log(` R2-D2 eye color is: ${character.eye_color}`);
+      }
     });
   } catch (error) {
-    console.log(error);
+    console.error("Something went wrong:", error);
   }
 }
 

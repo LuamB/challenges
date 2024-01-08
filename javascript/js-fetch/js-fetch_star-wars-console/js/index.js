2cde0660e1bc4a280e6f545646357df1744a3662
Luam Belay <luam.belay@gmail.com>
2024-01-08 15:40:57 +0100

    Star Wars API - Console

--- javascript/js-fetch/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -2,6 +2,25 @@ console.clear();
 
 const url = "https://swapi.dev/api/people";
 
-function fetchData() {}
+async function fetchData() {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("data: ", data);
+
+    const characters = data.results;
+    let charName = "";
+    let charEyeCol = "";
+
+    characters.forEach((character) => {
+      charName = character.name;
+      charEyeCol = character.eye_color;
+
+      console.log(`${charName} has ${charEyeCol} eyes`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 fetchData();

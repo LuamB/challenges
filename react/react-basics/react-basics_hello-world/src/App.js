e0e22abe0d854ff1af4cfdd83816cff7f6fd604b
Luam Belay <luam.belay@gmail.com>
2024-01-15 21:14:51 +0100

    Hello World

--- react/react-basics/react-basics_hello-world/src/App.js
+++ react/react-basics/react-basics_hello-world/src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./styles.css";
 
+const greeting = "Hello World!";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  // return <div>Say hello...</div>;
+  // return <h1>{greeting}</h1>;
+  return HelloWorld();
+}
+
+export function HelloWorld() {
+  return <h1>{greeting}</h1>;
 }

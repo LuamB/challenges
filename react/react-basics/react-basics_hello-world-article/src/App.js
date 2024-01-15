c8f28c7453108fbfb6902099a8b1cb13196fcb62
Luam Belay <luam.belay@gmail.com>
2024-01-15 21:37:58 +0100

    Hello world article

--- react/react-basics/react-basics_hello-world-article/src/App.js
+++ react/react-basics/react-basics_hello-world-article/src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  // return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  const title = "Hello World!";
+  const text = "Let's welcome the new week together 🤗";
+  const element = (
+    <article>
+      <h1>{title}</h1>
+      <p>{text}</p>
+    </article>
+  );
+  return element;
 }

3d21ac86a30cde33d35069ea366964c72c09dd5d
Luam Belay <luam.belay@gmail.com>
2024-01-16 00:51:46 +0100

    Smiley

--- react/react-props/react-props_smiley/src/App.js
+++ react/react-props/react-props_smiley/src/App.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  // return <h1>Replace me with your component!</h1>;
+  return <Smiley isHappy={false} />; //<- switch between true and false, here
 }
+
+const Smiley = ({ isHappy }) => {
+  const happy = "😃";
+  const unhappy = "😔";
+  return <h1>{isHappy ? happy : unhappy}</h1>;
+};

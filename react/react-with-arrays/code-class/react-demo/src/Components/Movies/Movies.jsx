00dd885096b0167a7f24aa18e028c39e77a46b11
Luam Belay <luam.belay@gmail.com>
2024-01-17 14:45:16 +0100

    code class

--- react/react-with-arrays/code-class/react-demo/src/Components/Movies/Movies.jsx
+++ react/react-with-arrays/code-class/react-demo/src/Components/Movies/Movies.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import './Movies.css';
+import React from "react";
+import "./Movies.css";
+import Card from "../MovieCard/Card";
 
-import { movies } from '../../Data/movies';
+import { movies } from "../../Data/movies";
 
 export default function Movies() {
-    console.log('Movies: ', movies);
-    return (
-        <main className='movies'>
-            <h1 className='movies__title'>Some great movies</h1>
-        </main>
-    );
+  console.log("Movies: ", movies);
+  return (
+    <main className="movies">
+      <h1 className="movies__title">Some great movies</h1>
+      {movies && movies.map((element)=> <Card title={element.title}/>)}
+    </main>
+  );
 }

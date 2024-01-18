622f334cd582334341e717dfb4452cd1094107b7
Luam Belay <luam.belay@gmail.com>
2024-01-18 10:14:12 +0100

    Adding react-state-2 files and code-class from react-with-arrays

--- react/react-with-arrays/code-class/react-demo/src/Components/Movies/Movies.jsx
+++ react/react-with-arrays/code-class/react-demo/src/Components/Movies/Movies.jsx
@@ -1,15 +1,21 @@
-import React from "react";
-import "./Movies.css";
-import Card from "../MovieCard/Card";
+import React from 'react';
+import './Movies.css';
 
-import { movies } from "../../Data/movies";
+import Card from '../MovieCard/Card';
+
+import { movies } from '../../Data/movies';
 
 export default function Movies() {
-  console.log("Movies: ", movies);
-  return (
-    <main className="movies">
-      <h1 className="movies__title">Some great movies</h1>
-      {movies && movies.map((element)=> <Card title={element.title}/>)}
-    </main>
-  );
+    console.log('Movies: ', movies);
+    return (
+        <main className='movies'>
+            <h1 className='movies__title'>Some great movies</h1>
+            {/* I want here to display my cards. */}
+            {/* If I use the {} in my arrow function I need the return */}
+            {movies &&
+                movies.map((element) => {
+                    return <Card movie={element} key={element.id} />;
+                })}
+        </main>
+    );
 }

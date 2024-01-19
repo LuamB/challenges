9f0ceb1714635db5427d87324f1e39f4d436fe68
Luam Belay <luam.belay@gmail.com>
2024-01-19 11:12:35 +0100

    Favourite Holiday Form

--- react/react-state-2/react-state-2_favourite-holiday/src/App.js
+++ react/react-state-2/react-state-2_favourite-holiday/src/App.js
@@ -2,11 +2,23 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [date, setDate] = useState("");
+  const [holiday, setHoliday] = useState("");
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    console.log("A new search term was submitted:", searchTerm);
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    // const formData = e.target.elements;
+    // console.log("Form data: ", formData);
+
+    // setDate(formData.date.value);
+    // setHoliday(formData.holiday.value);
+    const rawDate = e.target.elements.date.value;
+    const formattedDate = new Date(rawDate).toLocaleDateString();
+    console.log("formatted date: ", formattedDate);
+
+    setDate(formattedDate);
+    setHoliday(e.target.elements.holiday.value);
   }
 
   return (
@@ -24,8 +36,6 @@ export default function App() {
           type="text"
           name="holiday"
           placeholder="e.g. Christmas"
-          value={searchTerm}
-          onChange={}
         />
         <label htmlFor="date">Date: </label>
         <input id="date" type="date" name="date" />
@@ -35,10 +45,10 @@ export default function App() {
       </form>
       <h2>Output of Submitted Data</h2>
       <p>
-        Favourite Holiday: <span className="output">New Year</span>
+        Favourite Holiday: <span className="output">{holiday}</span>
       </p>
       <p>
-        Date: <span className="output">Well...</span>
+        Date: <span className="output">{date}</span>
       </p>
     </div>
   );

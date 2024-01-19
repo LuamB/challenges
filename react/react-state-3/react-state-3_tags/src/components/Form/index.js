10dda88a3992cb28a3e6171cd7ded1176665584d
Luam Belay <luam.belay@gmail.com>
2024-01-19 12:50:45 +0100

    Tags, tags, tags

--- react/react-state-3/react-state-3_tags/src/components/Form/index.js
+++ react/react-state-3/react-state-3_tags/src/components/Form/index.js
@@ -7,7 +7,7 @@ export default function Form({ onAddTag }) {
     const data = Object.fromEntries(formData);
 
     onAddTag(data.tag);
-
+    
     event.target.reset();
     event.target.elements.tag.focus();
   }

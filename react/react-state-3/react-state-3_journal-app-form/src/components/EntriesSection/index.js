1a35878f17893e3f096890a3ed0ad20c3a87c1a5
Luam Belay <luam.belay@gmail.com>
2024-01-19 13:35:44 +0100

    Life the state up

--- react/react-state-3/react-state-3_journal-app-form/src/components/EntriesSection/index.js
+++ react/react-state-3/react-state-3_journal-app-form/src/components/EntriesSection/index.js
@@ -4,42 +4,12 @@ import Entry from "../Entry";
 import Tabs from "../Tabs";
 import Tab from "../Tab";
 import Badge from "../Badge";
-import { useState, Fragment } from "react";
+import { Fragment } from "react";
 
-const initialEntries = [
-  {
-    id: 1000,
-    date: "Feb 5, 2025",
-    motto: "We are in a state of chaos",
-    notes:
-      "Today I learned about React State. It was fun! I can't wait to learn more.",
-  },
-  {
-    id: 999,
-    date: "Feb 4, 2025",
-    motto: "Props, Props, Props",
-    notes:
-      "Today I learned about React Props. Mad props to everyone who understands this!",
-  },
-  {
-    id: 998,
-    date: "Feb 3, 2025",
-    motto: "How to nest components online fast",
-    notes:
-      "Today I learned about React Components and how to nest them like a pro. Application design is so much fun!",
-  },
-  {
-    id: 997,
-    date: "Feb 2, 2025",
-    motto: "I'm a React Developer",
-    notes: "My React-ion when I learned about React: Yay!",
-  },
-];
+export default function EntriesSection({ entries }) {
+  // const [entries, setEntries] = useState(initialEntries);
+  // console.log("entries: ", entries);
 
-export default function EntriesSection() {
-  const [entries, setEntries] = useState(initialEntries);
-  console.log("entries: ", entries);
-  
   return (
     <section className="entries-section">
       <Tabs>
@@ -51,7 +21,7 @@ export default function EntriesSection() {
         </Tab>
       </Tabs>
       <div className="entries-section__entries">
-        {initialEntries.map((entry, index) => (
+        {entries.map((entry, index) => (
           <Fragment key={entry.id}>
             {index > 0 ? <Divider /> : null}
             <Entry date={entry.date} motto={entry.motto} notes={entry.notes} />

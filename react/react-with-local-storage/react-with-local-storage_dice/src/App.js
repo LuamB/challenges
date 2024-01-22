6740f0cd2de4f6ba07c5be0f6d193011ff49269c
Luam Belay <luam.belay@gmail.com>
2024-01-22 12:11:50 +0100

    Change D6Button component

--- react/react-with-local-storage/react-with-local-storage_dice/src/App.js
+++ react/react-with-local-storage/react-with-local-storage_dice/src/App.js
@@ -1,8 +1,18 @@
 import { D6Button } from "./components/D6Button";
 import History from "./components/History";
+import useLocalStorageState from "use-local-storage-state";
+import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [rolls, setRolls] = useState([]);
+
+  const handleRoll = () => {
+    setRolls([{ value: getD6Roll(), time: Date.now() }, ...rolls]);
+  };
+
+  const currentRollValue = rolls[0]?.value;
+
   return (
     <div className="app">
       <main className="app__main">

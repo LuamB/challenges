afd6164398ca366d87b70d2acf32cb7b2e4fb1e0
Luam Belay <luam.belay@gmail.com>
2024-01-24 17:03:20 +0100

    Overview Page Contents

---
+++ react/next/nextjs-basics-and-routing/lotr-app/pages/volumes.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Link from "next/link";
+import { volumes, introduction } from "@/resources/lib/data";
+
+export default function Volumes() {
+  return (
+    <>
+      <h1>Lord of the Rings</h1>
+      <p>{introduction}</p>
+      <h2>All Volumes</h2>
+      <ul>
+        {volumes.map((vol) => (
+          <li key={vol.slug}>
+            <Link href={`volumes/${vol.slug}`}>
+              <h3>{vol.title}</h3>{" "}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}

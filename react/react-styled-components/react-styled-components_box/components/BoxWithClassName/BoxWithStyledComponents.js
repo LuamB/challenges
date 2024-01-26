a798e57dec23e30fefd5bcb5ce1b38dd5b9976f7
Luam Belay <luam.belay@gmail.com>
2024-01-26 11:46:22 +0100

    React Styled Components - Box

---
+++ react/react-styled-components/react-styled-components_box/components/BoxWithClassName/BoxWithStyledComponents.js
@@ -0,0 +1,18 @@
+import styled, { css } from "styled-components";
+
+const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: green;
+  margin: 2rem;
+
+  ${({ $isBlack }) =>
+    $isBlack &&
+    css`
+      background-color: black;
+    `}
+`;
+
+export default function BoxWithStyledComponents({ isBlack }) {
+  return <StyledBox $isBlack={isBlack}></StyledBox>;
+}

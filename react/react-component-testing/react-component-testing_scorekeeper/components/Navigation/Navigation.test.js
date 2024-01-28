6c6267e7b17c597740e530fe49658d4ddc1b894d
Luam Belay <luam.belay@gmail.com>
2024-01-28 22:34:37 +0100

    Implemented successful Navigation test

--- react/react-component-testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
+++ react/react-component-testing/react-component-testing_scorekeeper/components/Navigation/Navigation.test.js
@@ -8,11 +8,14 @@ jest.mock("next/router", () => ({
 }));
 
 test("renders with two links 'Play' and 'History'", () => {
+  // Render the Navigation component
   render(<Navigation players={[]} />);
 
+  // Check rendering of 'Play' & 'History' links, using the role "link"
   const playLink = screen.getByRole("link", { name: /Play/i });
   const historyLink = screen.getByRole("link", { name: /History/i });
 
+  // Check presence of links in document
   expect(playLink).toBeInTheDocument();
   expect(historyLink).toBeInTheDocument();
 });

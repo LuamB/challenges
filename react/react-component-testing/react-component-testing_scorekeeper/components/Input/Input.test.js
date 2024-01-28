dc34706de277c886c0acf088c14fa99732dfcb34
Luam Belay <luam.belay@gmail.com>
2024-01-28 22:48:42 +0100

    Implemented first successful Input test

--- react/react-component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
+++ react/react-component-testing/react-component-testing_scorekeeper/components/Input/Input.test.js
@@ -2,6 +2,26 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Input from ".";
 
-test("renders a label and an input with the correct attributes", () => {});
+test("renders a label and an input with the correct attributes", () => {
+  // Test data
+  const labelText = "Test label";
+  const placeholder = "Test placeholder";
+  const name = "testName";
+  const value = "Test Value";
+  const required = true;
+  const handleChange = jest.fn();
+
+  // Render Input component
+  render(
+    <Input
+      labelText={labelText}
+      placeholder={placeholder}
+      name={name}
+      value={value}
+      onChange={handleChange}
+      required={required}
+    />
+  );
+});
 
 test("calls callback on every user input", async () => {});

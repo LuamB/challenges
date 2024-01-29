55f09a82af983333f828e9c33f53fa44851afeab
Luam Belay <luam.belay@gmail.com>
2024-01-29 12:06:03 +0100

    Adding react-global-state + react-styled-components code-from-lecture and solutions

---
+++ react/react-styled-components/solutions/solutions/react-styled-components_box/pages/index.js
@@ -0,0 +1,16 @@
+import Box from "../components/BoxWithClassName/BoxWithStyledComponents.js";
+import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import styled from "styled-components"; 
+export default function HomePage() {
+ 
+  const Container = styled.div`
+    display: flex;
+  `
+  return (
+    <Container>
+      <BoxWithClassName />
+      <Box isBlack={true}/>
+      <Box/>
+    </Container>
+  );
+}

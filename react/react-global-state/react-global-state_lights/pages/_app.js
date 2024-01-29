99b8fe9ce6eb5a965a8df4d16b818c3a7ce1a186
Luam Belay <luam.belay@gmail.com>
2024-01-29 23:01:29 +0100

    Lifting state up & counting the lights

--- react/react-global-state/react-global-state_lights/pages/_app.js
+++ react/react-global-state/react-global-state_lights/pages/_app.js
@@ -1,11 +1,61 @@
+import { useState } from "react";
 import GlobalStyle from "../styles";
 import Layout from "../components/Layout";
 
+const initLights = [
+  { id: 1, isOn: false, name: "Living Room" },
+  { id: 2, isOn: false, name: "Kitchen" },
+  { id: 3, isOn: false, name: "Bedroom" },
+  { id: 4, isOn: false, name: "Bathroom" },
+  { id: 5, isOn: false, name: "Garage" },
+  { id: 6, isOn: false, name: "Porch" },
+  { id: 7, isOn: false, name: "Garden" },
+  { id: 8, isOn: false, name: "Office" },
+];
+
 export default function App({ Component, pageProps }) {
+  // const [isOn, setIsOn] = useState(false);
+  const [lights, setLights] = useState(initLights);
+
+  const lightsOnCount = lights.filter((light) => light.isOn === true).length;
+
+  const isDimmed =
+    lights.filter((light) => light.isOn === false).length === lights.length;
+
+  function handleToggle(lightId) {
+    const updatedLights = lights.map((light) =>
+      light.id === lightId ? { ...light, isOn: !light.isOn } : light
+    );
+    setLights(updatedLights);
+  }
+
+  function handleAllOn() {
+    const updatedLights = lights.map((light) => {
+      return { ...light, isOn: true };
+    });
+    setLights(updatedLights);
+  }
+
+  function handleAllOff() {
+    const updatedLights = lights.map((light) => {
+      return { ...light, isOn: false };
+    });
+    setLights(updatedLights);
+  }
+
   return (
-    <Layout>
+    <Layout isDimmed={isDimmed}>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component
+        {...pageProps}
+        lights={lights}
+        lightsOnCount={lightsOnCount}
+        toggleLight={handleToggle}
+        handleAllOn={handleAllOn}
+        handleAllOff={handleAllOff}
+      />
     </Layout>
   );
 }
+
+// () => {handleToggle()}

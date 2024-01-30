7915f0cea0f910ea442af3c0258a8493d7472690
Luam Belay <luam.belay@gmail.com>
2024-01-30 19:37:38 +0100

    Dynamic route for fetched character

---
+++ react/react-data-fetching/react-data-fetching_star-wars/pages/characters/[id].js
@@ -0,0 +1,55 @@
+import Card from "../../components/Card";
+import Layout from "../../components/Layout";
+import useSWR from "swr";
+import { useRouter } from "next/router";
+
+const fetcher = async (url) => {
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    const error = new Error("An error occured while fetching the data.");
+    error.info = await res.json();
+    error.status = res.status;
+    throw error;
+  }
+
+  return res.json();
+};
+
+export default function Character() {
+  // get id
+  const router = useRouter();
+  const { id } = router.query;
+
+  // fetch data per character
+  const { data, isLoading, error } = useSWR(
+    `https://swapi.dev/api/people/${id}`,
+    fetcher
+  );
+  console.log("data: ", data);
+
+  // loading & error msg
+  if (isLoading)
+    return (
+      <h1 style={{ textAlign: "center", marginTop: "50vh" }}>Loading...</h1>
+    );
+  if (error)
+    return (
+      <h1 style={{ textAlign: "center", marginTop: "50vh" }}>
+        {error.message}
+      </h1>
+    );
+
+  // rendering
+  return (
+    <Layout>
+      <Card
+        id={id}
+        name={data.name}
+        height={data.height}
+        eyeColor={data.eye_color}
+        birthYear={data.birth_year}
+      />
+    </Layout>
+  );
+}
